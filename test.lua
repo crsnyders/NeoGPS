0f86fdde5b3fc18a3dc51fccb4148027b824b7c1
Christopher Snyders <csnyders@thefieldoffice.co.za>
2016-02-12 14:21:03 +0200

    basic json recieve

--- test.lua
+++ test.lua
@@ -1,12 +1,12 @@
 -- a simple http server
 handleRquest =function(conn,payload)
-  jsonPayload = cjson.decode(payload);
-  for k,v in pairs(t) do print(k,v) end
-end
-
-connectionHandler =function(conn)
-    conn:on("receive",handleRquest(conn,payload))
-end
+  if payload ~= nil then
+    jsonPayload = cjson.decode(payload);
+    for k,v in pairs(jsonPayload) do print(k,v) end
+   end
+ end
 
 srv=net.createServer(net.TCP)
-srv:listen(80,connectionHandler(conn));
+srv:listen(80,function(conn)
+    conn:on("receive",handleRquest)
+end)
